0068d4f9d1610c10e36e090ec242cc224af78d91
Milan Steiner <steiner.milan@hotmail.com>
2022-02-09 16:03:36 +0100

    Fix linting errors

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,11 +1,11 @@
-import {
-  toAscii,
-  fromAscii,
-  toUtf8,
-  fromUtf8,
-  toBase64,
-  fromBase64,
-  toHex,
-  fromHex,
-  Bech32,
+export {
+    toAscii,
+    fromAscii,
+    toUtf8,
+    fromUtf8,
+    toBase64,
+    fromBase64,
+    toHex,
+    fromHex,
+    Bech32,
 } from "@cosmjs/encoding";
