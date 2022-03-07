adde2cab3935bf16c406bfae1e464354eaa59878
Milan Steiner <steiner.milan@hotmail.com>
2022-03-07 10:13:20 +0100

    Add UMD Module

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require("webpack");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const commonConfig = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp:
+        /wordlists\/(french|spanish|italian|korean|chinese_simplified|chinese_traditional|japanese)\.json$/,
+    }),
+  ],
+};
+
+const webConfig = {
+  ...commonConfig,
+  target: "web",
+  output: {
+    filename: "bundle.js",
+    libraryTarget: "umd",
+    library: "CosmWasmJS",
+  },
+  resolve: {
+    ...commonConfig.resolve,
+    fallback: {
+      stream: require.resolve("stream-browserify"),
+      buffer: require.resolve("buffer"),
+      path: require.resolve("path-browserify"),
+    },
+  },
+  plugins: [
+    ...commonConfig.plugins,
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
+
+const nodeConfig = {
+  ...commonConfig,
+  target: "node",
+  output: {
+    libraryTarget: "commonjs",
+    filename: "bundle.node.js",
+  },
+};
+
+module.exports = [webConfig, nodeConfig];
